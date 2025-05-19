df1c9bbfcf1189aba3905a2dca5c2ddb30085e19
saad1551 <saad.ashraf.9094@gmail.com>
2025-05-19 05:00:57 +0500

    update app

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from llm import generate_reply
 
 app = FastAPI()
 
 @app.post("/llm")
 def llm(message: str):
-    return 
+    reply = generate_reply(message)
+
+    return {
+        "message": reply
+    }
